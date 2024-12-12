7eb0ba6d2a7f609b6d7e4db342068ed86dff1eb2
Wirer0 <ggrzybek246@gmail.com>
2024-12-12 18:57:07 +0100

    poxniejtozrobie

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import StronaGlowna from './components/StronaGlowna';
-
+import Todolist from './components/Todolist/Todolist';
 function App() {
   return (
     <div className="App">
         <StronaGlowna/>
+        <Todolist/>
     </div>
   );
 }
---
+++ src/components/Todolist/todolist.jsx
@@ -0,0 +1,23 @@
+function Todolist(){
+    
+    
+    
+    
+    
+    
+    return <div>
+            <div class="sidebar">
+<div class="user-info">
+  </div>
+  <button class="dodawanie">+Dodaj Zadanie </button>
+  <ul class="menu">
+    <li>Szukaj</li>
+    <li>Dzisiaj</li>
+    <li>Nadchodzące Wydarzenia</li>
+  </ul>
+</div></div>
+}
+
+
+
+export default Todolist

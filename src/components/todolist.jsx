60f70c3844f8ce5d007c7df2b4cdfa3590ff9308
PoK728 <siaziawiktor@gmail.com>
2024-12-12 20:36:10 +0100

    sie wywaliło xD

---
+++ src/components/todolist.jsx
@@ -0,0 +1,14 @@
+function Todolist(){
+  return <div>
+    <div class="sidebar">
+      <div class="user-info"></div>
+      <button class="dodawanie">+Dodaj Zadanie </button>
+      <ul class="menu">
+        <li>Szukaj</li>
+        <li>Dzisiaj</li>
+        <li>Nadchodzące Wydarzenia</li>
+      </ul>
+    </div>
+</div>
+}
+export default Todolist
